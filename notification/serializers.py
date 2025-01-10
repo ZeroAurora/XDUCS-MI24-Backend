e232df02fb44ee56fb0016c04f7d6998dd0f0285
ZeroAurora <25359455+ZeroAurora@users.noreply.github.com>
2025-01-10 22:50:36 +0800

    clean up notification serializer

--- notification/serializers.py
+++ notification/serializers.py
@@ -1,19 +1,16 @@
 from rest_framework import serializers
 from .models import Notification
-from user.serializers import ProfileSerializer
 from post.serializers import ContentSerializer
 
 
 class NotificationSerializer(serializers.ModelSerializer):
-    profile = serializers.SerializerMethodField(read_only=True)
-    content = ContentSerializer(read_only=True)
-
+    source_user = serializers.SerializerMethodField(read_only=True)
+    
     class Meta:
         model = Notification
         fields = [
             "id",
-            "user",
-            "profile",
+            "source_user",
             "content",
             "notification_type",
             "created_at",
@@ -21,5 +18,5 @@ class NotificationSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ["created_at", "is_read"]
 
-    def get_profile(self, obj):
-        return ProfileSerializer(obj.user.profile).data
+    def get_source_user(self, obj):
+        return obj.content.user
